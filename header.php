e291698a2305383d37ff7410fc1773e50c2bedae
GiovannyJara <giovanny-296@hotmail.com>
2019-05-17 21:13:15 -0500

    version_3

---
+++ header.php
@@ -0,0 +1,17 @@
+
+<!doctype html>
+
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/style.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/slick.css">
+
+	<title><?php bloginfo ('name'); echo " | "; bloginfo ('description'); ?></title>
+	<?php wp_head(); ?>
+	
+</head>
+<body>
